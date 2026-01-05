d4618765371814489743a14c8a9c2fdaca2f816d
Joseph Koch <joseph.koch55@gmail.com>
2026-01-05 15:52:03 -0500

    Add shortcuts for vim-fugitive

--- nvim/after/plugin/vim-fugitive.lua
+++ nvim/after/plugin/vim-fugitive.lua
@@ -1,1 +1,31 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+
+vim.api.nvim_create_user_command(
+    "Ga",
+    function(opts)
+        if #opts.fargs == 0 then
+            vim.cmd("G add .")
+        else
+            vim.cmd("G add " .. table.concat(opts.fargs, " "))
+        end
+    end,
+    { nargs = "*" }
+)
+
+vim.api.nvim_create_user_command(
+    "Gc",
+    function(opts)
+        vim.cmd("G commit -m " .. vim.fn.shellescape(opts.args))
+    end,
+    { nargs = 1 }
+)
+
+vim.api.nvim_create_user_command(
+    "Gp",
+    function(opts)
+      vim.cmd("G push")
+    end,
+    { nargs = 0 }
+)
+
+
