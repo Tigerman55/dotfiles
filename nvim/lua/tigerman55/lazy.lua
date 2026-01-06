2ae8ce85ae9661db12fdcbebc5f6475aa1c30d03
Joseph Koch <joseph.koch55@gmail.com>
2026-01-06 09:49:22 -0500

    "Set up dadbod"

--- nvim/lua/tigerman55/lazy.lua
+++ nvim/lua/tigerman55/lazy.lua
@@ -185,6 +185,23 @@ require("lazy").setup({
     {
         "stevearc/oil.nvim",
         dependencies = {{ "nvim-mini/mini.icons", opts = {} }},
-    }
+    },
+    {
+        "tpope/vim-dadbod",
+        cmd = { "DB", "DBUI", "DBUIToggle" }
+    },
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        dependencies = { "tpope/vim-dadbod" },
+        cmd = { "DBUI", "DBUIToggle" },
+        init = function()
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
+    },
+    {
+        "kristijanhusak/vim-dadbod-completion",
+        dependencies = { "tpope/vim-dadbod" },
+        ft = { "sql", "mysql" },
+    },
 })
 
