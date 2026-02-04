e613498f3a054e28b0abbbca14e9cb5af0e28daf
Joseph Koch <joseph.koch55@gmail.com>
2026-02-04 10:35:54 -0500

    Improve ts indentation config

--- nvim/after/ftplugin/ts.lua
+++ nvim/after/ftplugin/ts.lua
@@ -1,5 +1,2 @@
-vim.opt_local.expandtab = false  -- use tabs
-vim.opt_local.tabstop = 4       -- how wide a tab looks
-vim.opt_local.shiftwidth = 4   -- indent size
-vim.opt_local.softtabstop = 0  -- keep tabs "real"
-
+vim.opt_local.tabstop = 4         -- Number of spaces a Tab in the file counts for
+vim.opt_local.shiftwidth = 4      -- Number of spaces to use for each step of (auto)indent
--- nvim/lua/tigerman55/vim-options.lua
+++ nvim/lua/tigerman55/vim-options.lua
@@ -25,3 +25,4 @@ vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
 vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
 
 vim.opt.mouse = ""
+vim.opt.startofline = false
