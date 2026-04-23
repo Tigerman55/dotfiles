3d5327192d78d58e4bd71c0bdafa4695914a03cf
Joseph Koch <joseph.koch55@gmail.com>
2026-04-23 17:14:04 -0400

    Allow oil.nvim to make lsp renames

--- nvim/after/plugin/oil.lua
+++ nvim/after/plugin/oil.lua
@@ -1,10 +1,14 @@
 require("oil").setup({
-    cleanup_delay_ms = 5 * 60 * 1000, -- 5 minutes
     default_file_explorer = true,
     keymaps = {
         ["<C-p>"] = false,
     },
     view_options = {
         show_hidden = true,
+    },
+    lsp_file_methods = {
+        enabled = true,
+        timeout_ms = 2 * 1000,
+        autosave_changes = 'unmodified'
     }
 });
